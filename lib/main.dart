b321ea04b7a68ff64afd70cbeb0cf571e3d8dc69
ali kazemi <alikazemidev@gmail.comi>
2022-03-31 19:00:32 +0430

    refactor some code

--- lib/main.dart
+++ lib/main.dart
@@ -107,20 +107,6 @@ class MainContent extends StatefulWidget {
 class _MainContentState extends State<MainContent> {
   int _currentIndex = 0;
 
-  final List<String> imagesList = [
-    'https://cdn.pixabay.com/photo/2020/11/01/23/22/breakfast-5705180_1280.jpg',
-    'https://cdn.pixabay.com/photo/2016/11/18/19/00/breads-1836411_1280.jpg',
-    'https://cdn.pixabay.com/photo/2019/01/14/17/25/gelato-3932596_1280.jpg',
-    'https://cdn.pixabay.com/photo/2017/04/04/18/07/ice-cream-2202561_1280.jpg',
-  ];
-
-  final List<String> titles = [
-    ' Coffee ',
-    ' Bread ',
-    ' Gelato ',
-    ' Ice',
-  ];
-
 // *get header
   Widget _getHeader() {
     return Row(
